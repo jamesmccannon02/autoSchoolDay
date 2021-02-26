fc60df6da12be71ab9d0da91ffa498de00981737
James McCannon <jamesmccannon@James-Mac-mini.local>
2021-02-26 19:05:23 +0000

    std_day operation

---
+++ autoSchoolDay.py
@@ -0,0 +1,23 @@
+import sys
+from os import system
+import logging
+
+def std_day():
+    logging.info("Opening applications")
+    system("open /Applications/Microsoft\ Teams.app/")
+    system("open /System/Applications/Mail.app/")
+
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="autoSchoolDay.log")
+    try:
+        if len(sys.argv) > 1:
+            print(sys.argv)
+        else:
+            std_day()
+    except:
+        logging.error("Operation did not work")
+        print("An error has occurred")
+
