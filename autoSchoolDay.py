5a1322754883e01a23f55fbcc9506ea847a0fbda
James McCannon <jamesmccannon@Jamess-MacBook-Pro-3.local>
2021-02-27 01:17:30 +0000

    Doc change, app change

--- autoSchoolDay.py
+++ autoSchoolDay.py
@@ -29,7 +29,7 @@ def python():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="autoSchoolDay.log")
+    logging.basicConfig(filename="/autoProjects/autoSchoolDay/autoSchoolDay.log")
     try:
         if len(sys.argv) > 1:
             if sys.argv[1] == "engine":
