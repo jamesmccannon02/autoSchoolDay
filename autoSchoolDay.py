1d08f6c93c99c9407c0039717bb84615d98fd4a6
James McCannon <jamesmccannon@James-Mac-mini.local>
2021-03-01 21:08:46 +0000

    Now using argparse

--- autoSchoolDay.py
+++ autoSchoolDay.py
@@ -1,21 +1,24 @@
+#!/usr/local/bin/python3
+
 import sys
 from os import system
 
+import argparse
 
 def std_day():
     system("open /Applications/Microsoft\ Teams.app/")
     system("open /System/Applications/Mail.app/")
 
 
-def engine():
-    system("open /Documents/school/Year 12_&_13/Engineering")
+def engineering():
+    system("open /Documents/school/Year 12\_&\_13/Engineering")
 
 
-def bus():
+def business():
     system("open ~/Documents/school/Year\ 12_\&_13/Business")
 
 
-def cs():
+def computer_science():
     system("open ~/Documents/school/Year\ 12_\&_13/Computer\ science/")
 
 
@@ -24,17 +27,24 @@ def python():
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="This allows quick opening of applications used within the school day")
+
+    parser.add_argument('school', choices=['start', 'engine', 'bus', 'cs', 'python'])
+
+    args = parser.parse_args()
+
     try:
-        if len(sys.argv) > 1:
-            if sys.argv[1] == "engine":
-                engine()
-            elif sys.argv[1] == "cs":
-                cs()
-            elif sys.argv[1] == "python":
+        if args.school:
+            if args.school == "engine":
+                engineering()
+            elif args.school == "cs":
+                computer_science()
+            elif args.school == "python":
                 python()
-            elif sys.argv[1] == "bus":
-                bus()
-        else:
-            std_day()
-    except:
-        print("An error has occurred")
+            elif args.school == "bus":
+                business()
+            elif args.school == "start":
+                std_day()
+    except Exception as e:
+        print("An error has occurred", e)
